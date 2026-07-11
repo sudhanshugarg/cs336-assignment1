9d48bf5979bb39024055d4672a1bc50e20013dc3
Sudhanshu Garg <sudgarg@paypal.com>
2026-07-10 17:44:03 -0700

    fixing reader

--- src/hf_reader.py
+++ src/hf_reader.py
@@ -1,10 +1,13 @@
 from datasets import load_dataset, Dataset, load_from_disk
 
-def get_sample_records(ds, n: int = 1000):
+def get_sample_records(ds, n: int = 0):
     # records = ds.select(range(n))
     print(ds.column_names)
     # print(len(ds))
-    records = ds.take(n)
+    if n > 0:
+        records = ds.take(n)
+    else:
+        records = ds
     return records
 
 def write_sample_records(ds, path):
@@ -12,18 +15,16 @@ def write_sample_records(ds, path):
     with open(path, "w") as f:
         for row in ds:
             text = row["text"].strip()
-            f.write(f"{text}\n{endToken}\n")
+            f.write(f"{text}{endToken}\n")
 
-def generate_tinystores():
-    ds = load_dataset("roneneldan/TinyStories", split="train")
-    n = 1000
+def generate_tinystores(n: int = 0):
+    ds = load_dataset("roneneldan/TinyStories", split="train", streaming=False)
     ds = get_sample_records(ds, n)
     write_sample_records(ds, f"src/resources/tinystories_sample_{n}.bin")
 
 
-def generate_openwebtext():
-    ds = load_dataset("Skylion007/openwebtext", split="train", streaming=True)
-    n = 300_000
+def generate_openwebtext(n: int = 0):
+    ds = load_dataset("Skylion007/openwebtext", split="train", streaming=False)
     ds = get_sample_records(ds, n)
     write_sample_records(ds, f"src/resources/openwebtext_sample_{n}.bin")
 
