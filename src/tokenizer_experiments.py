0abcec681bf53c255be8689ea2539f46a6412acf
Sudhanshu Garg <sudgarg@paypal.com>
2026-07-10 20:58:32 -0700

    making the tokenizer work

--- src/tokenizer_experiments.py
+++ src/tokenizer_experiments.py
@@ -2,9 +2,10 @@ from cs336_basics.pretokenization_example import find_chunk_boundaries
 from src.tokenizer import Tokenizer
 import numpy as np
 
-vocab_path = "src/resources/tinystories_vocab_10000.pkl"
-input_text_path = "src/resources/openwebtext_sample_2000.bin"
-output_tokens_path = "src/resources/openwebtext_tokenized_2000.bin"
+vocab_path = "src/resources/openwebtext_vocab_32000_1000.pkl"
+input_text_path = "src/resources/openwebtext_sample_0.bin"
+output_tokens_path_1 = "src/resources/openwebtext_full_tokenized.bin"
+output_tokens_path_np = "src/resources/openwebtext_full_tokenized"
 
 tokenizer = Tokenizer.from_files(
                 vocab_filepath=vocab_path,
@@ -14,35 +15,38 @@ tokenizer = Tokenizer.from_files(
 
 
 with open(input_text_path, "rb") as f:
-    num_processes = 1
+    num_processes = 1000
     boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
-    print(boundaries)
+    # print(boundaries)
 
     output_arr = np.memmap(
-        output_tokens_path,
+        output_tokens_path_1,
         dtype=np.uint16,
         mode="w+",
         shape=(boundaries[-1],)
     )
     # The following is a serial implementation, but you can parallelize this
     # by sending each start/end pair to a set of processes.
-    i = 0
+    i, j = 0, 1
     for start, end in zip(boundaries[:-1], boundaries[1:]):
         f.seek(start)
         chunk = f.read(end - start).decode("utf-8", errors="ignore")
         encoded_tokens = np.array(tokenizer.encode(chunk), dtype=np.uint16)
-        output_arr[i:len(encoded_tokens)] = encoded_tokens
+        output_arr[i:i+len(encoded_tokens)] = encoded_tokens
         i += len(encoded_tokens)
+        print(f"got {len(encoded_tokens)} from {j}th chunk")
         output_arr.flush()
+        j += 1
 
     length = i
     print(f"num_tokens = {length}")
 
 arr2 = np.memmap(
-    output_tokens_path,
+    output_tokens_path_1,
     dtype=np.uint16,
     mode="r",
     shape=(length,)
 )
 
+np.save(output_tokens_path_np, arr2)
 print(len(arr2))
